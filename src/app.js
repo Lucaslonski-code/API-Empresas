2d85536807f77228d9a548c0a34577b59507d06b
Lucaslonski-code <lucaslonski890@gmail.com>
2026-06-04 11:35:27 -0300

    First commit, first files, and the port is working fine.

---
+++ src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.json({
+    message: "Servidor funcionando"
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Servidor rodando na porta ${process.env.PORT}`
+  );
+});
